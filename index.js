dad4d119512f234a188bb4809054142fcc7f1e19
Milind <milindmishra11@gmail.com>
2023-12-17 05:17:51 +0530

    put and delete req with conditions added

--- index.js
+++ index.js
@@ -41,17 +41,92 @@ const express = require('express')
 
 const app = express();
 
+
+//Anytime we restart the process the in memory data is reset . that is why we use databases
 var users = [{
     name: "John",
     kidneys: [{
-        healthy: false
+        healthy: false,
+        
     }]
 }];
+
+
+app.use(express.json());
+
 
 app.get("/", (req,res) => {
     const johnKidneys = users[0].kidneys
     const numberOfKidneys = johnKidneys.length;
-    console.log(numberOfKidneys)
+    let numberOfHealthyKidneys = 0;
+    for (let i = 0; i < numberOfKidneys; i++) {
+        if (johnKidneys[i].healthy) {
+            numberOfHealthyKidneys++;
+        }
+    }
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        johnKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
 })
+
+app.post("/", (req,res) => {
+    //for post request we send data in the body of the request
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
+})
+
+
+
+app.put("/", (req,res) => {
+    for( let i = 0; i < users.length; i++) {
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({
+        msg: "ok   Done!"
+    })
+})
+
+
+app.delete("/", (req,res) => {
+
+    // only if atleast one unhealthy kidney is there do this , else return 411
+    
+    if(isThereAtleastOneUnhealthyKidney()){
+        const newKidneys = [];
+        for (let i = 0; i < users[0].kidneys.length; i++) {
+          if (users[0].kidneys[i].healthy) {
+            newKidneys.push({
+              healthy: true,
+            });
+          }
+        }
+        users[0].kidneys = newKidneys;
+        res.json({
+          msg: "okay",
+        });
+    } else {
+        res.status(411).json({
+            msg: "You dont have any unhealthy kidneys"
+        })
+    }
+});
+
+function isThereAtleastOneUnhealthyKidney() {
+    let atleastOneUnhealthyKidney = false;
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+      if (!users[0].kidneys[i].healthy) {
+        atleastOneUnhealthyKidney = true;
+      }
+    }
+    return atleastOneUnhealthyKidney;
+}
 
 app.listen(3000)
